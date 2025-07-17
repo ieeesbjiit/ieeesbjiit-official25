6dae8023cac5997e5c27cf75fbc85ee930de7ade
Pranshu Sharma <pranshusharmaindia@gmail.com>
2025-07-17 21:06:22 +0530

    stats change done

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -21,7 +21,7 @@ const statsData = [
     icon: "bi-people",
     color: "#0d6efd",
     target: 3000,
-    label: "Attendees Acroos Events",
+    label: "Attendees Across Events",
     img: "attendees.webp",
   },
   {
